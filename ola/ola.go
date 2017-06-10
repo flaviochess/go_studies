7af0ebc35994cc61362755d504ab5d5b4a8188fe
Flavio Andrade Machado <flaviochess@gmail.com>
2017-06-10 17:35:43 -0300

    first commit

---
+++ ola/ola.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/flaviochess/newmath"
+)
+
+func main() {
+	fmt.Printf("olá, mundo\n")
+	fmt.Printf("Raiz quadrada de 2. Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
